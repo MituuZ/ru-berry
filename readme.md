49e0c5ae371050be41ada13ffe866f846c7a20aa
Mitja Leino <mitja.leino@hotmail.com>
2024-12-22 12:49:48 +0200

    Add timeout and retry

--- readme.md
+++ readme.md
@@ -3,6 +3,9 @@ Rust application for integrating Zigbee devices designed to run on a Raspberry P
 
 Initial goal is to persist and visualize temperature and humidity data.
 
+r2d2 is used for a connection pool, which only contains a single connection to the SQLite database.
+This way there is no need to worry about async issues with SQLite.
+
 ## Stack
 - [Rust](https://www.rust-lang.org/)
 - [Mosquitto](https://mosquitto.org/) 
@@ -12,3 +15,4 @@ Initial goal is to persist and visualize temperature and humidity data.
   - `features = ["bundled"]` handles installing SQLite
 - [SQLite](https://www.sqlite.org/index.html)
 - [Serde](https://serde.rs/)
+- [r2d2](https://github.com/sfackler/r2d2)
--- src/conn.rs
+++ src/conn.rs
@@ -1,17 +1,45 @@
-use r2d2::{Pool, PooledConnection};
+use std::fmt::{Debug, Formatter};
+use std::time::Duration;
+use r2d2::{CustomizeConnection, Pool, PooledConnection};
 use r2d2_sqlite::SqliteConnectionManager;
 
 pub type SqlitePool = Pool<SqliteConnectionManager>;
 pub type SqlitePooledConnection = PooledConnection<SqliteConnectionManager>;
 
+struct RetryConnectionCustomizer {
+    retries: i32
+}
+
 pub fn create_pool(database_url: &str) -> SqlitePool {
     let manager = SqliteConnectionManager::file(database_url);
     Pool::builder()
         .max_size(1) // Single connection
+        .connection_timeout(Duration::from_secs(5))
+        .connection_customizer(Box::new(RetryConnectionCustomizer { retries: 3 }))
         .build(manager)
         .expect("Failed to create pool.")
 }
 
 pub fn get_conn(pool: &SqlitePool) -> SqlitePooledConnection {
     pool.get().expect("Failed to get connection.")
+}
+
+impl Debug for RetryConnectionCustomizer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "RetryConnectionCustomizer {{ retries: {} }}", self.retries)
+    }
+}
+
+impl CustomizeConnection<rusqlite::Connection, rusqlite::Error> for RetryConnectionCustomizer {
+    fn on_acquire(&self, conn: &mut rusqlite::Connection) -> Result<(), rusqlite::Error> {
+        for _ in 0..self.retries {
+            if conn.is_autocommit() {
+                return Ok(());
+            }
+        }
+        Err(rusqlite::Error::SqliteFailure(
+            rusqlite::ffi::Error::new(rusqlite::ffi::SQLITE_BUSY),
+            Some("Failed to acquire connection after retries".to_string())
+        ))
+    }
 }
