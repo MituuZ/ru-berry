ce01375a8ad8c4ecf28a4eeb54ae7679eabca781
Mitja Leino <mitja.leino@hotmail.com>
2024-12-26 10:07:59 +0200

    Map humidity and temp data for the last 3 days

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,4 @@
+use chrono::{Local, NaiveDateTime, TimeZone, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -11,12 +12,18 @@ pub struct SensorData {
 
 impl SensorData {
     pub fn from_row(row: &rusqlite::Row) -> rusqlite::Result<Self> {
+        let received_at: String = row.get(5)?;
+        let received_at_naive = NaiveDateTime::parse_from_str(&received_at, "%Y-%m-%d %H:%M:%S")
+            .expect("Failed to parse received_at");
+        let received_at_utc = Utc.from_utc_datetime(&received_at_naive);
+        let received_at_with_tz = received_at_utc.with_timezone(&Local);
+
         Ok(SensorData {
             temperature: row.get::<_, f64>(1)? as f32,
             humidity: row.get(2)?,
             linkquality: row.get(3)?,
             device_id: row.get(4)?,
-            received_at: row.get(5)?,
+            received_at: received_at_with_tz.to_string(),
         })
     }
 }
--- src/web/status.rs
+++ src/web/status.rs
@@ -1,4 +1,3 @@
-use chrono::{Local, NaiveDateTime, TimeZone, Utc};
 use crate::conn::{get_conn, SqlitePool};
 use crate::model::SensorData;
 use crate::web::ru_berry_web::MyError;
@@ -8,66 +7,84 @@ pub async fn get_sensor_data_status(pool: SqlitePool) -> Result<impl warp::Reply
     println!("Getting sensor data");
 
     let mut stmt = match conn.prepare(
-        "select * from sensor_data where received_at \
-    >= datetime('now', '-3 days') order by temperature asc limit 1;",
+        "SELECT * FROM (
+            SELECT *, 1 as data_type FROM (
+                SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY temperature ASC LIMIT 1
+            )
+            )
+            UNION
+            SELECT * FROM (
+                SELECT *, 2 as data_type FROM (
+                    SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY humidity ASC LIMIT 1
+                )
+            )
+            UNION
+            SELECT * FROM (
+                SELECT *, 3 as data_type FROM (
+                    SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY humidity DESC LIMIT 1
+                )
+            )
+            ORDER BY data_type;"
     ) {
         Ok(stmt) => stmt,
         Err(_) => return Err(warp::reject::custom(MyError::QueryPreparationError)),
     };
 
-    let min_temp_data = match stmt.query_map([], |row| SensorData::from_row(row)) {
+    let query_data = match stmt.query_map([], |row| SensorData::from_row(row)) {
         Ok(iter) => iter,
         Err(_) => return Err(warp::reject::custom(MyError::QueryExecutionError)),
     };
 
-    let sensor_data = match min_temp_data.collect::<Result<Vec<SensorData>, rusqlite::Error>>() {
+    let sensor_data = match query_data.collect::<Result<Vec<SensorData>, rusqlite::Error>>() {
         Ok(data) => data,
         Err(_) => return Err(warp::reject::custom(MyError::DataMappingError)),
     };
 
-    // Inside the map function
-    let received_at_naive = NaiveDateTime::parse_from_str(&sensor_data[0].received_at, "%Y-%m-%d %H:%M:%S")
-        .expect("Failed to parse received_at");
-    let received_at_utc = Utc.from_utc_datetime(&received_at_naive);
-    let received_at_with_tz = received_at_utc.with_timezone(&Local);
+    if sensor_data.len() == 0 {
+        return Ok(warp::reply::html("No data found"));
+    }
 
     let html = format!(
         "<html>
         <head><title>Sensor Data Status</title></head>
         <body>
             <h1>Sensor Data Status</h1>
-            <h2>Minimum Temperature in the Last 3 Days</h2>
-            <table border=\"1\">
-                <tr>
-                    <th>Temperature</th>
-                    <th>Humidity</th>
-                    <th>Link Quality</th>
-                    <th>Device ID</th>
-                    <th>Received At</th>
-                </tr>
-                {}
-            </table>
-        </body>
-    </html>",
-        sensor_data
-            .iter()
-            .map(|data| format!(
-                "<tr>
-            <td>{}</td>
-            <td>{}</td>
-            <td>{}</td>
-            <td>{}</td>
-            <td>{}</td>
-        </tr>",
-                data.temperature,
-                data.humidity,
-                data.linkquality,
-                data.device_id,
-                received_at_with_tz
-            ))
-            .collect::<String>()
+            {}
+            {}
+            {}",
+        create_table(&sensor_data[0], "Lowest Temperature"),
+        create_table(&sensor_data[1], "Lowest Humidity"),
+        create_table(&sensor_data[2], "Highest Humidity")
     );
 
+    let html: &'static str = Box::leak(html.into_boxed_str());
     Ok(warp::reply::html(html))
 }
 
+fn create_table(sensor_data: &SensorData, title: &str) -> String {
+    format!(
+        "<h2>{}</h2>\
+        <table border = \"1\">\
+        <tr>\
+            <th>Temperature</th>\
+            <th>Humidity</th>\
+            <th>Link Quality</th>\
+            <th>Device ID</th>\
+            <th>Received At</th>\
+        </tr>\
+        <tr>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+        </tr>
+        </table>",
+        title,
+        sensor_data.temperature,
+        sensor_data.humidity,
+        sensor_data.linkquality,
+        sensor_data.device_id,
+        sensor_data.received_at
+    )
+}
