96c3f1ca9f6aa6547d796f4ce13acec4f5b99aad
Mitja Leino <mitja.leino@hotmail.com>
2024-12-26 10:36:20 +0200

    Include status titles in the queries for clarity

--- src/web/status.rs
+++ src/web/status.rs
@@ -7,52 +7,53 @@ pub async fn get_sensor_data_status(pool: SqlitePool) -> Result<impl warp::Reply
     println!("Getting sensor data");
 
     let mut stmt = match conn.prepare(
-        "SELECT * FROM (
-                SELECT *, 1 as data_type FROM (
-                    SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY temperature ASC LIMIT 1
-                )
+        "
+            SELECT *, 1 as data_type, 'Lowest Temperature in the Last 3 Days' as title FROM (
+                SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY temperature ASC LIMIT 1
             )
             UNION
-            SELECT * FROM (
-                SELECT *, 2 as data_type FROM (
-                    SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY humidity DESC LIMIT 1
-                )
+            SELECT *, 2 as data_type, 'Highest Temperature in the Last 3 Days' as title FROM (
+                SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY temperature DESC LIMIT 1
             )
             UNION
-            SELECT * FROM (
-                SELECT *, 3 as data_type FROM (
-                    SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY humidity ASC LIMIT 1
-                )
+            SELECT *, 3 as data_type, 'Lowest Humidity in the Last 3 Days' as title FROM (
+                SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY humidity ASC LIMIT 1
             )
             UNION
-            SELECT * FROM (
-                SELECT *, 4 as data_type FROM (
-                    SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY humidity DESC LIMIT 1
-                )
+            SELECT *, 4 as data_type, 'Highest Humidity in the Last 3 Days' as title FROM (
+                SELECT * FROM sensor_data WHERE received_at >= datetime('now', '-3 days') ORDER BY humidity DESC LIMIT 1
             )
             UNION
-            SELECT * FROM (
-                SELECT *, 5 as data_type FROM (
-                    SELECT * FROM sensor_data order by received_at DESC LIMIT 1
-                )
+            SELECT *, 5 as data_type, 'Latest Reading' as title FROM (
+                SELECT * FROM sensor_data ORDER BY received_at DESC LIMIT 1
             )
-            ORDER BY data_type;"
+            ORDER BY data_type;
+            "
     ) {
         Ok(stmt) => stmt,
         Err(_) => return Err(warp::reject::custom(MyError::QueryPreparationError)),
     };
 
-    let query_data = match stmt.query_map([], |row| SensorData::from_row(row)) {
+    let query_data = match stmt.query_map([], |row| {
+        let sensor_data = SensorData::from_row(row)?;
+        let title: String = row.get("title")?;
+        Ok((sensor_data, title))
+    }) {
         Ok(iter) => iter,
         Err(_) => return Err(warp::reject::custom(MyError::QueryExecutionError)),
     };
 
-    let sensor_data = match query_data.collect::<Result<Vec<SensorData>, rusqlite::Error>>() {
-        Ok(data) => data,
-        Err(_) => return Err(warp::reject::custom(MyError::DataMappingError)),
-    };
+    let mut sensor_data_with_titles = Vec::new();
+    for result in query_data {
+        match result {
+            Ok((data, title)) => {
+                sensor_data_with_titles.push((data, title));
+            }
+            Err(_) => return Err(warp::reject::custom(MyError::DataMappingError)),
+        }
+    }
 
-    if sensor_data.len() == 0 {
+    if sensor_data_with_titles.len() == 0 {
         return Ok(warp::reply::html("No data found"));
     }
 
@@ -75,15 +76,11 @@ pub async fn get_sensor_data_status(pool: SqlitePool) -> Result<impl warp::Reply
         <body>
             <h1>Sensor Data Status</h1>
             {}
-            {}
-            {}
-            {}
-            {}",
-        create_table(&sensor_data[0], "Lowest Temperature in the Last 3 Days"),
-        create_table(&sensor_data[1], "Highest Temperature in the Last 3 Days"),
-        create_table(&sensor_data[2], "Lowest Humidity in the Last 3 Days"),
-        create_table(&sensor_data[3], "Highest Humidity in the Last 3 Days"),
-        create_table(&sensor_data[4], "Latest Reading")
+            ",
+        sensor_data_with_titles
+            .iter()
+            .map(|(data, title)| create_table(data, title))
+            .collect::<String>()
     );
 
     let html: &'static str = Box::leak(html.into_boxed_str());
